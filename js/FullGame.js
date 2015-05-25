39b7ab9c61e428076244c26c489ed6fe11882681
ubak <ubak_1@hotmail.com>
2015-05-25 16:47:00 +0200

    pruebas con this

--- js/FullGame.js
+++ js/FullGame.js
@@ -2,18 +2,22 @@ var FullGame = function(){
     
     this.cursor = game.input.keyboard.createCursorKeys();
     
-    background = game.add.sprite(0,0, 'background');
-    var card = null;
-    var deck = new Deck_fire();
+    this.background = game.add.sprite(0,0, 'background');
+    this.card = null;
+    this.deck = new Deck_fire();
+    
+    this.card = this.deck.deck_1[1];
+    
+    console.log(this.card);
     
     this.update = function() {
         
         //para mover la carta una vez seleccionada
-        if(card != null) deck.setPosition(card);
+        if(this.card != null) this.deck.setPosition(this.card);
         
         //prueva de que si se pulsa la tecla up (despues se camibara por el click del raton) selecciona la carta deseada
         if (this.cursor.up.isDown) {
-            card = deck.checkMousePos(game.input.mousePointer.x, game.input.mousePointer.y);
+            this.card = this.deck.checkMousePos(game.input.mousePointer.x, game.input.mousePointer.y);
 		}
     };
     
--- js/deck/decks/Deck_fire.js
+++ js/deck/decks/Deck_fire.js
@@ -1,26 +1,26 @@
 var Deck_fire = function(){
     
-    var patrulla_de_trasgos = new Patrulla_de_trasgos();
+    //var patrulla_de_trasgos = new Patrulla_de_trasgos();
     
     //creamos un mazo de 10 cartas
-    var deck = [10];
+    this.deck_1 = [20];
 
     // rellenamos el mazo con los trasgos para hacer pruevas
     for (var i = 1; i < 10; i++){
-        deck[i] = patrulla_de_trasgos;
+        this.deck_1[i] = new Patrulla_de_trasgos();   
     }
-
+    
     //para cambiar la posicion de la carta seleccionada
     this.setPosition = function(card){
-       deck[card].setPos(game.input.activePointer.x, game.input.activePointer.y);
+       this.card.setPos(game.input.activePointer.x, game.input.activePointer.y);
     }
     
     //para comprobar si el mouse esta encima de alguna de las cartaas
     this.checkMousePos = function(posX, posY){
-        for(var j = 0; j < deck.length; j ++){
-            if(posX >= deck[j].getPosX && posX <= deck[j].getPosX + 100 && posY >= deck[j].getPosY && posY <= deck[j].getPosY + 100){
+        for(var j = 0; j < this.deck.length; j ++){
+            if(posX >= this.deck_1[j].getPosX && posX <= this.deck_1[j].getPosX + 100 && posY >= this.deck_1[j].getPosY && posY <= this.deck_1[j].getPosY + 100){
                 console.log("encontrado");
-                return deck[j];
+                return this.deck_1[j];
             }
         }   
     }
